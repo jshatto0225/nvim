ec28ca66f5781149a0647fdb8f629a2630bf2ba8
jshatto0225 <jshatto0225@gmail.com>
2025-01-28 19:10:15 -0500

    nvim

--- lua/jshatto0225/lazy/colors.lua
+++ lua/jshatto0225/lazy/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-  color = color or "rose-pine-moon"
+  color = color or "tokyonight"
   vim.cmd.colorscheme(color)
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -9,7 +9,6 @@ function ColorMyPencils(color)
 end
 
 return {
-
   {
     "erikbackman/brightburn.vim",
   },
